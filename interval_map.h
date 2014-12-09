0604ffe2f59477e45ba967d8655f5223b26693e0
Sean Ogden <positivelyskewed.sf@gmail.com>
2014-12-09 11:44:08 -0500

    compiles.

--- interval_map.h
+++ interval_map.h
@@ -45,6 +45,27 @@ class interval_map
         interval_map(const interval_map&);
 
     private:
+        void insert_contained (
+                unsigned int block_start_address,
+                unsigned int block_length,
+                unsigned int insert_address,
+                unsigned int insert_length);
+        void insert_right(
+                unsigned int block_start_address,
+                unsigned int insert_address);
+        void insert_left(
+                unsigned int block_start_address,
+                unsigned int block_length,
+                unsigned int insert_address,
+                unsigned int insert_length);
+        void insert_overwrite_interval(
+                unsigned int block_start_address);
+        void insert_interval(
+                unsigned int insert_address,
+                unsigned int insert_length,
+                block_location insert_location);
+
+    private:
         typedef std::map<unsigned int, slice> slice_map_t;
         typedef std::map<unsigned int, slice>::iterator slice_iter_t;
         interval_map& operator = (const interval_map&);
