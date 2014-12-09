c5a9ecc334d399a773bcaac97c8d0c72db4c8dc1
Sean Ogden <positivelyskewed.sf@gmail.com>
2014-12-09 10:32:03 -0500

    added class prefix.

--- interval_map.cc
+++ interval_map.cc
@@ -1,13 +1,13 @@
 #include "interval_map.h"
 using interval_map;
 
-interval_map::interval_map()
+interval_map :: interval_map()
     : map()
 {
 
 }
 
-interval_map::~interval_map()
+interval_map :: ~interval_map()
 {
 
 }
@@ -16,7 +16,7 @@ interval_map::~interval_map()
 
 
 void 
-interval_map::insert(unsigned int insert_address, unsigned int insert_length)
+interval_map :: insert(unsigned int insert_address, unsigned int insert_length)
 {
         
     //case 1: wholely contained within a larger block
@@ -27,38 +27,44 @@ interval_map::insert(unsigned int insert_address, unsigned int insert_length)
     //case 6: larger than a whole block (combo of 2, 3 and 5)
 }
 
-void insert_contained(
+void 
+interval_map :: insert_contained(
     unsigned int block_start_address,
     unsigned int block_length,
     unsigned int insert_address,
     unsigned int insert_length);
 
-void insert_end(
+void 
+interval_map :: insert_end(
     unsigned int block_start_address,
     unsigned int block_length, // not needed
     unsigned int insert_address
     unsigned int insert_length // not needed
     );
 
-void insert_front(
+void 
+interval_map :: insert_front(
     unsigned int block_start_address,
     unsigned int block_length,
     unsigned int insert_address,
     unsigned int insert_length);
 
-void insert_eof(
+void 
+interval_map :: insert_eof(
     unsigned int block_start_address,
     unsigned int block_length,
     unsigned int insert_address,
     unsigned int insert_length);
 
-void insert_overwrite_interval(
+void 
+interval_map :: insert_overwrite_interval(
     unsigned int block_start_address,
     unsigned int block_length,
     unsigned int insert_address,
     unsigned int insert_length);
 
-void insert_interval(
+void 
+interval_map :: insert_interval(
     unsigned int insert_address,
     unsigned int insert_length
     );
